c17f22e423b2419775e9f1dafc921a4010f972ef
saputraananda <saputraananda70@gmail.com>
2026-09-17 15:12:30 +0700

    fix : bug's deployment

---
+++ dist/sw.js
@@ -0,0 +1,125 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "index.html",
+    "revision": "d9c3d4698341da7233b95311c9445979"
+  }, {
+    "url": "assets/workbox-window.prod.es5-BBnX5xw4.js",
+    "revision": null
+  }, {
+    "url": "assets/index-B0VXMcQa.css",
+    "revision": null
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "2810063dbe097906fc5c63a9338b79d4"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "cea2092582875b3b1b81118c1ed40944"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "9d0a0d535c9c0e5d6add02fb4d607464"
+  }, {
+    "url": "splash-1640x2360.png",
+    "revision": "2884ea3cad22e76a6677b975266afee3"
+  }, {
+    "url": "splash-1668x2388.png",
+    "revision": "ef408097fa421b9bc7ecc243343a5d55"
+  }, {
+    "url": "splash-2048x2732.png",
+    "revision": "4f5fbaae46ef9b372b8bf9ea5c56ae0c"
+  }, {
+    "url": "splash-2360x1640.png",
+    "revision": "9c89644d2f776027e624a5ff5ece4536"
+  }, {
+    "url": "splash-2388x1668.png",
+    "revision": "b6e20d1142c37078fa079a42dedf5d8d"
+  }, {
+    "url": "splash-2732x2048.png",
+    "revision": "06e17fd99a987c8df22d62f1bbcd7d3e"
+  }, {
+    "url": "waschen.webp",
+    "revision": "ddd1c5be5155ebf971d805c6db1eeb87"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "b458fece4be6f84cee6f57e6ad344045"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
